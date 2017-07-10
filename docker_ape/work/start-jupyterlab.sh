42b66ee0a1037ca4eb2a23f5c852e92a2c168775
Bibo Hao <haobibo@users.noreply.github.com>
2017-07-10 13:55:44 +0800

    fix start script path

--- docker_ape/work/start-jupyterlab.sh
+++ docker_ape/work/start-jupyterlab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source start--pre.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/start--pre.sh
 
 notebook_arg=""
 ( vergte `jupyter notebook --version` 5.0.0 ) && notebook_arg="${notebook_arg} --allow-root"
