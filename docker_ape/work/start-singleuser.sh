2d9d492ff5636380ddbadc15d3c652d2a8b29f07
Bibo Hao <haobibo@users.noreply.github.com>
2017-08-29 01:05:57 +0800

    CMD: single-user -> notebook

--- docker_ape/work/start-singleuser.sh
+++ docker_ape/work/start-singleuser.sh
@@ -6,13 +6,6 @@ notebook_arg=""
 
 ( ver_gte `jupyter notebook --version` 5.0.0 ) && notebook_arg="${notebook_arg} --allow-root"
 [ -n "${USE_SSL:+x}" ] && notebook_arg="${notebook_arg} --NotebookApp.certfile=${NOTEBOOK_PEM_FILE}"
-[ -n "${NOTEBOOK_DIR:+x}" ] && notebook_arg="${notebook_arg} --notebook-dir=${NOTEBOOK_DIR}"
 
-exec jupyterhub-singleuser --ip=0.0.0.0 --port=8888 \
-  --user=$JPY_USER \
-  --cookie-name=$JPY_COOKIE_NAME \
-  --base-url=$JPY_BASE_URL \
-  --hub-prefix=$JPY_HUB_PREFIX \
-  --hub-api-url=$JPY_HUB_API_URL \
-  ${notebook_arg} \
-  $@
+
+exec jupyterhub-singleuser --ip=0.0.0.0 --port=8888 ${notebook_arg} $@
